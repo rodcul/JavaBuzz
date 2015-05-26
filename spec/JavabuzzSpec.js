41b148b597ac813723d26c42be5e0a91b6fd4ede
Rodney Cullen <rodcul@gmail.com>
2015-05-26 20:26:13 +0100

    git init

---
+++ spec/JavabuzzSpec.js
@@ -0,0 +1,18 @@
+describe('Javabuzz', function() {
+  var javabuzz;
+
+  describe('knows when a number is', function(){
+    it('divisible by 3', function(){
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is NOT', function(){
+    it('divisible by 3', function() {
+    javabuzz = new Javabuzz();
+    expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+  });
+});
+
+});
---
+++ src/Javabuzz.js
@@ -0,0 +1,6 @@
+var Javabuzz = function(){
+
+  Javabuzz.prototype.isDivisibleByThree = function (number) {
+    return (number % 3 === 0);
+  };
+};
