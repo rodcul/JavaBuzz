60805403cb5220f28769b5abdc17929aba656485
Rodney Cullen <rodcul@gmail.com>
2015-05-26 20:38:10 +0100

    Added test and for  5 & 15

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -1,18 +1,45 @@
 describe('Javabuzz', function() {
   var javabuzz;
 
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+
   describe('knows when a number is', function(){
     it('divisible by 3', function(){
-      javabuzz = new Javabuzz();
-      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+      expect(javabuzz._isDivisibleBy(3,3)).toBe(true);
     });
   });
 
   describe('knows when a number is NOT', function(){
     it('divisible by 3', function() {
-    javabuzz = new Javabuzz();
-    expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    expect(javabuzz._isDivisibleBy(1,3)).toBe(false);
   });
 });
+
+  describe('knows when a number is', function(){
+    it('divisible by 5', function(){
+      expect(javabuzz._isDivisibleBy(5,5)).toBe(true);
+    });
+});
+
+  describe('knows when a number is NOT', function(){
+    it('divisible by 5', function() {
+    expect(javabuzz._isDivisibleBy(1,5)).toBe(false);
+  });
+});
+
+describe('knows when a number is', function(){
+  it('divisible by 3 & 5', function(){
+    expect(javabuzz._isDivisibleBy(15,15)).toBe(true);
+  });
+});
+
+describe('knows when a number is NOT', function(){
+  it('divisible by 3 & 5', function() {
+  expect(javabuzz._isDivisibleBy(1,15)).toBe(false);
+});
+});
+
 
 });
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -1,6 +1,6 @@
 var Javabuzz = function(){
 
-  Javabuzz.prototype.isDivisibleByThree = function (number) {
-    return (number % 3 === 0);
-  };
+  Javabuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return (number % divisor === 0);
+};
 };
